77c2bf636d29f5fe8d0bd7d01c3e25007644ece3
CodeBuilder <m49686629@gmail.com>
2026-03-22 22:46:10 -0500

    2.0.4.1 J.js

--- J.js
+++ J.js
@@ -369,7 +369,7 @@ function accountOlderThan(createdAt, seconds) {
 const HARD_BLOCKED_WORDS = [
     "nigger", "nigga", "kike", "spic", "chink", "wetback",
     "faggot", "tranny", "nonce", "cock", "rape", "pussy",
-    "rapist", "nazi", "hitler", "dick", "dicks", "dick", "kill", "d¡ck", "k¡ll"
+    "rapist", "nazi", "hitler", "dick", "dicks", "dick", "kill", "d¡ck", "k¡ll", "die", "d¡e"
 ];
 
 function containsProfanity(text) {
